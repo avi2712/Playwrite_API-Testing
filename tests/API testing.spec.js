82ae7f834a5864c5dc4076f9e8f0fcb8d747c3b9
avinash <avinashchaugule11@gmail.com>
2026-07-16 14:37:34 +0530

    first commit umesh

---
+++ tests/API testing.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('TC_001 Register with valid data', async ({ request }) => {
+
+  const response = request.post('https://api.eventhub.rahulshettyacademy.com/api/auth/register',
+    {
+      headers:{
+        'accept':'application/json',
+        'content-type':'application/json'
+      },
+      data:{
+        'email':'test297@gmail.com',
+        'password':'test25081997'
+      }
+    }
+  );
+
+  expect((await response).status()).toBe(201);
+
+});
